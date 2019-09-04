0db82bf3e43cbc11c4be987015b6e2853d8a4286
Tim Allen <tnelsallen3@gmail.com>
2019-09-04 16:22:21 -0600

    Add additional statistics to comedians index

--- app/models/special.rb
+++ app/models/special.rb
@@ -2,4 +2,12 @@ class Special < ApplicationRecord
   belongs_to :comedian
 
   validates_presence_of :name, :runtime
+
+  def self.specials_count(com_ids)
+    where(comedian_id: com_ids).count
+  end
+
+  def self.avg_run_time(com_ids)
+    where(comedian_id: com_ids).average(:runtime)
+  end
 end
